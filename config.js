27b2a1b760a99e1af69cbf78410b1f313c6fe813
Ömer Akdoğan <akdoganomer42621@gmail.com>
2025-07-21 17:17:41 +0300

    aSas

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const SIRKET_BILGI = {
     ad: "ÖMER AKDOĞAN Servis LTD. ŞTİ.",
     vergiNo: "1234567890",
-    adres: "Çiftlikköy Mahallesi Atatürk Caddesi No:10, Yenişehir/MERSİ7N",
+    adres: "Çiftlikköy Mahallesi Atatürk Caddesi No:10, Yenişehir/MERSİN",
     konum: {
       lat: 36.7884118,
       lng: 34.5203594
     },
     telefon: "+90 (537) 204 01 91",
-    email: "akdoganomer42621@gmail.com",
+    email: "wsqwjhfsqwjhss@gmail.com",
     siteAdi: "Beyaz Eşya Servisi",
     siteAciklama: "Buzdolabı, çamaşır makinesi, klima ve kombi için garantisiz teknik servis hizmeti.",
     footerUyari: "Bu web sitesi yalnızca garantisi olmayan ürünlere hizmet sunmaktadır. Yetkili servis değildir.",
