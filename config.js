fb7e3b2bb942eaee5ce0b31ce81a3480ac197aae
Ömer Akdoğan <akdoganomer42621@gmail.com>
2025-07-22 09:59:30 +0300

    faag

--- config.js
+++ config.js
@@ -12,14 +12,5 @@ const SIRKET_BILGI = {
     siteAciklama: "Buzdolabı, çamaşır makinesi, klima ve kombi için garantisiz teknik servis hizmeti.",
     footerUyari: "Bu web sitesi yalnızca garantisi olmayan ürünlere hizmet sunmaktadır. Yetkili servis değildir.",
     saatler: "Pzt-Cmt: 09:00 - 19:00 | Pazar: Kapalı",
-    sosyal: {
-      facebook: "https://www.facebook.com/omer.canpolat.16568/?locale=tr_TR",
-      instagram: "https://www.instagram.com/akdoganomer21/",
-      twitter: "https://x.com/OmerA56731",
-      whatsapp: "https://web.whatsapp.com/",
-      telegram: "https://web.telegram.org/a/",
-      linkedin: "https://www.linkedin.com/in/%C3%B6mer-akdo%C4%9Fan-a8a373259/",
-      youtube: "https://www.youtube.com/@omerakdogan8597"
-    }
   };
   
